576f419d40e9d83fd92972010a3dd1fbd89ce560
g-freire <gustavomfreire@gmail.com>
2019-11-10 22:36:32 -0300

    add dbms book code reference

--- DMBS-5.20-QUERY.sql
+++ DMBS-5.20-QUERY.sql
@@ -1,4 +1,5 @@
 /*  
+	Database Management Systems 2nd Edition - 2000
 	5.2 - THE FORM OF A BASIC SQL QUERY
 	
 	This section presents the syntax of  asimple SQL query 
@@ -11,28 +12,109 @@
 
 */  
 
-use master
+
+-- DDL - Domain Definition Language
+
+BEGIN TRANSACTION TCreateDB  
+
+	use master
+	go
+
+	create database BoatTourBusiness2
+	go 
+
+COMMIT TRAN TCreateDB;
+
+
+
+BEGIN TRANSACTION TCreateTables  
+
+	use BoatTourBusiness2
+	go
+
+	create table Sailors
+	(
+	  sid int ,
+	  sname varchar(50),
+	  rating int,
+	  age float
+	);
+	go
+
+	create table Reserves
+	(
+	  sid int ,
+	  bid int,
+	  day varchar(50),
+	  --day DateTime,
+	);
+	go
+
+	create table Boats
+	( bid  int,
+	  bname varchar(50),
+	  color varchar(50)
+	)
+	go
+
+--ROLLBACK TRAN @TransactionName;  
+COMMIT TRAN TCreateTables;
 go
 
---create database BoatTourStore
-use BoatTourStore
 
-user master
-drop database BoatTourStore
+BEGIN TRANSACTION TPopulateTable;  
 
-SP_WHO 
- alter database BoatTourStore set single_user with rollback immediate
+	insert into Sailors(sid,sname,rating,age)
+	values
+			(22, 'Dustin', 7, 45.0),
+			(29, 'Brutus', 1, 33.0),
+			(31, 'Lubber', 8, 55.5),
+			(32, 'Andy', 8, 25.5),
+			(58, 'Rusty', 10, 35.5),
+			(64, 'Horatio', 7, 35.0),
+			(71, 'Zorba', 10, 16.0),
+			(74, 'Horation', 9, 35.0),
+			(85, 'Art', 3, 25.5),
+			(95, 'Bob', 3, 63.5)
 
- drop databaseBoatTourStore
-
-KILL 59
-
-BoatTourStore
 
 
-DECLARE @kill varchar(8000) = '';  
-SELECT @kill = @kill + 'kill ' + CONVERT(varchar(5), session_id) + ';'  
-FROM sys.dm_exec_sessions
-WHERE database_id  = db_id('BoatTourStore')
+	insert into Boats(bid, bname, color) 
+	values 
+			(101, 'Interlake', 'blue'),
+			(102, 'Interlake', 'red'),
+			(103, 'Clipper', 'green'),
+			(104, 'Marine', 'red')
 
-EXEC(@kill);
+
+
+	insert into Reserves(sid, bid, day)
+	values 
+			(22, 101, '10/10/98'),
+			(22, 102, '10/10/98'),
+			(22, 103, '10/8/98'),
+			(22, 101, '10/7/98'),
+			(31, 101, '11/10/98'),
+			(31, 101, '11/6/98'),
+			(31, 101, '11/12/98'),
+			(64, 101, '9/5/98'),
+			(64, 101, '9/8/98'),
+			(74, 101, '9/8/98')
+
+
+----ROLLBACK TRAN T1  
+COMMIT TRAN TPopulateTable;
+go
+
+
+select * from Sailors
+select * from Reserves
+select * from Boats 
+
+begin tran deleteTables
+	drop table Sailors
+	drop table Reserves
+	drop table Boats
+commit tran
+
+
